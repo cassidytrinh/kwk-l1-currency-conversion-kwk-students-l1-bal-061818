94cd0c53369ebf1ebf995e65bfe5788230e760fb
cassidytrinh <cassidytrinh@gmail.com>
2018-06-19 19:09:52 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,1 +1,26 @@
-# Write your cod here
+def usd_to_eur(val)
+  return val/1.2
+end
+
+def eur_to_usd(val)
+  return val*1.2
+end
+
+def usd_to_jpy(val)
+end
+
+def usd_to_gpb(val)
+end 
+
+def usd_to_aud(val)
+end
+
+def jpy_to_usd(val)
+end
+
+def gbp_to_usd(val)
+end
+
+def aud_to_usd(val)
+end
+

57fa8145d5d26c697a890bb0315c7b1ed509924c
cassidytrinh <cassidytrinh@gmail.com>
2018-06-19 19:18:44 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -33,7 +33,7 @@ end
 puts "How much money do you want to convert?"
 amount = gets.chomp().to_i
 
-puts "Pick from the following menu:"
+puts "Enter a test value between 1 and 8 twice to test:"
 puts "1. USD to EUR"
 puts "2. USD to JPY"
 puts "3. USD to GBP"
